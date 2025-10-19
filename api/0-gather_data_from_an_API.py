54e5ab8353e634da22c828a9d9255703ddd39d04
Nsh-teddy <t.nshimyumu@alustudent.com>
2025-10-19 08:27:45 +0200

    Fix final PEP8 line length and blank line issue

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,7 +7,9 @@ import sys
 if __name__ == "__main__":
     user_id = int(sys.argv[1])
     user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
-    todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
+    todos_url = (
+        f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
+    )
 
     user = requests.get(user_url).json()
     todos = requests.get(todos_url).json()
@@ -18,7 +20,8 @@ if __name__ == "__main__":
     num_done_tasks = len(done_tasks)
 
     print(
-        f"Employee {employee_name} is done with tasks({num_done_tasks}/{total_tasks}):"
+        f"Employee {employee_name} is done with tasks "
+        f"({num_done_tasks}/{total_tasks}):"
     )
     for task in done_tasks:
         print(f"\t {task.get('title')}")
