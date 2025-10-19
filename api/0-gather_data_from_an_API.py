4e58b0e3f6247bb271de833acced2895a7b56532
Nsh-teddy <t.nshimyumu@alustudent.com>
2025-10-19 08:25:32 +0200

    Fix PEP8 line length and trailing blank line

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
-"""Script that, using a REST API, returns information about an employee's TODO list progress"""
+"""Script that uses a REST API to get employee TODO list progress."""
 
 import requests
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        user_id = sys.argv[1]
-        base_url = "https://jsonplaceholder.typicode.com"
+    user_id = int(sys.argv[1])
+    user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+    todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
 
-        # Get user info
-        user = requests.get(f"{base_url}/users/{user_id}").json()
-        employee_name = user.get("name")
+    user = requests.get(user_url).json()
+    todos = requests.get(todos_url).json()
 
-        # Get todos
-        todos = requests.get(f"{base_url}/todos?userId={user_id}").json()
-        done_tasks = [task for task in todos if task.get("completed")]
+    employee_name = user.get("name")
+    total_tasks = len(todos)
+    done_tasks = [t for t in todos if t.get("completed") is True]
+    num_done_tasks = len(done_tasks)
 
-        print(f"Employee {employee_name} is done with tasks({len(done_tasks)}/{len(todos)}):")
-        for task in done_tasks:
-            print(f"\t {task.get('title')}")
+    print(
+        f"Employee {employee_name} is done with tasks({num_done_tasks}/{total_tasks}):"
+    )
+    for task in done_tasks:
+        print(f"\t {task.get('title')}")
