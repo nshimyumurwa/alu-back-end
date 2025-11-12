9df7af7384ff18c1216665667b5da5d8e50bbe39
nshimyumurwa <marynshimyumurwa@gmail.com>
2025-11-12 15:38:14 +0000

    Fix PEP8 violations - remove trailing whitespace and fix long lines

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,34 +5,35 @@ Script that fetches TODO list progress for a given employee ID from an API
 import requests
 import sys
 
-
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         sys.exit(1)
-    
+
     try:
         employee_id = int(sys.argv[1])
     except ValueError:
         sys.exit(1)
-    
+
     base_url = "https://jsonplaceholder.typicode.com"
-    
-    user_response = requests.get("{}/users/{}".format(base_url, employee_id))
-    todos_response = requests.get("{}/todos?userId={}".format(base_url, employee_id))
-    
-    if user_response.status_code != 200 or todos_response.status_code != 200:
+    user_response = requests.get("{}/users/{}".format(base_url,
+                                                       employee_id))
+    todos_response = requests.get("{}/todos?userId={}".format(base_url,
+                                                               employee_id))
+
+    if (user_response.status_code != 200 or
+            todos_response.status_code != 200):
         sys.exit(1)
-    
+
     user_data = user_response.json()
     todos_data = todos_response.json()
-    
+
     employee_name = user_data.get("name")
     total_tasks = len(todos_data)
     completed_tasks = [task for task in todos_data if task.get("completed")]
     num_completed = len(completed_tasks)
-    
+
     print("Employee {} is done with tasks({}/{}):".format(
         employee_name, num_completed, total_tasks))
-    
+
     for task in completed_tasks:
         print("\t {}".format(task.get('title')))
